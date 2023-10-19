186db891ddc7b3454fff8b62d41d329374361eef
p2nts <gabrieltucker260@gmail.com>
2023-10-19 14:45:45 -0700

    Add more functionality and class options.
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,14 @@ function handleClassPicker(event) {
 
     if (height >= 6 && height < 8 && gender === "male"){
         result = "Knight"
-    } else if (height >= 4 && height <= 6 && species === "Elf" && gender === "female" || gender === "nb" || gender === "other" ) {
+    } else if ((height >= 4 && height <= 6) && (species === "Elf") && (gender === "female" || gender === "nb" || gender === "other")) {
         result = "Archer"
-    }
-
+    } else if ((height >= 6 && height <= 10) && (species === "Elf" || species === "Human"))
+        result = "Mage"
+        else if ((height >= 3 && height < 6) && (species ==="Human" || species === "Orc"))
+        result = "Thief"
+    else
+    result = "Villager"
 
     document.getElementById("output").innerText = result;
 }
